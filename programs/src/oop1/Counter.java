e0b9c69b53005d62985e50c7df6721d88b4b3022
srikanthpragada <srikanthpragada@gmail.com>
2024-05-08 18:55:58 +0530

    08-MAY

---
+++ programs/src/oop1/Counter.java
@@ -0,0 +1,16 @@
+package oop1;
+
+public class Counter {
+	// instance variable
+	private int value;
+
+	// Methods 
+	public void inc() {
+		value++;
+	}
+
+	public void dec() {
+		value--;
+	}
+
+}
---
+++ programs/src/oop1/TestCounter.java
@@ -0,0 +1,22 @@
+package oop1;
+
+public class TestCounter {
+
+	public static void main(String[] args) {
+	     Counter c;  // object reference 
+	     
+	     c = new Counter(); // Create object 
+	     
+	     c.inc();
+	     c.inc();
+	     
+	     c.dec();
+	     
+	     //System.out.println(c.value);
+	     
+	     
+	     
+
+	}
+
+}
